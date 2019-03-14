d4b99d6f7d3d167ac48064c3346cb966bb9a483f
mey1k <mey1k@naver.com>
2019-03-14 11:16:22 +0900

    기능명 : 프로젝트 생성 수정사항 : 없음

---
+++ custommaterialwidget.cpp
@@ -0,0 +1,6 @@
+#include "custommaterialwidget.h"
+
+
+CustomMaterialWidget::CustomMaterialWidget()
+{
+}
---
+++ custommaterialwidget.h
@@ -0,0 +1,12 @@
+#ifndef CUSTOMMATERIALWIDGET_H
+#define CUSTOMMATERIALWIDGET_H
+
+
+class CustomMaterialWidget
+{
+
+public:
+    CustomMaterialWidget();
+};
+
+#endif // CUSTOMMATERIALWIDGET_H
